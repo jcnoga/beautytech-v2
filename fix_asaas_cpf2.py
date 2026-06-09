a425be9e0d30cb882aa70b1c2dfeac214577a4d0
Jose Carlos <jcnvap@gmail.com>
2026-06-09 12:50:25 -0300

    feat: trial banner + upgrade button + plan locks + PWA

--- fix_asaas_cpf2.py
+++
@@ -1,8 +0,0 @@
-content = open('C:/projetos/beautytech-v2/backend/src/modules/asaas.module.ts', encoding='utf-8').read()
-old = '    await asaasFetch("PUT", `/customers/${customerId}`, { cpfCnpj: "00000000000" });'
-new = '    await asaasFetch("PUT", `/customers/${customerId}`, { cpfCnpj: "24971563792" });'
-if old in content:
-    open('C:/projetos/beautytech-v2/backend/src/modules/asaas.module.ts', 'w', encoding='utf-8').write(content.replace(old, new, 1))
-    print('CORRIGIDO')
-else:
-    print('TRECHO NAO ENCONTRADO')
