c4583d172cb54297cd7cbecbe1eed28d621da6c2
Jose Carlos <jcnvap@gmail.com>
2026-06-11 10:06:58 -0300

    fix: novo service id emailjs service_70lgemm

---
+++ fix_sid.py
@@ -0,0 +1,9 @@
+﻿# -*- coding: utf-8 -*-
+for fname in [r"index.html", r"frontend\public\landing.html"]:
+    with open(fname, "r", encoding="utf-8") as f:
+        content = f.read()
+    content = content.replace("service_pis825m", "service_70lgemm")
+    content = content.replace("service_htsrx6q", "service_70lgemm")
+    with open(fname, "w", encoding="utf-8") as f:
+        f.write(content)
+    print(f"OK {fname} atualizado!")
