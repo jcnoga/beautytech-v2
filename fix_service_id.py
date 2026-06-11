42326625055e1e4fd557845d63b7b3077095ce84
Jose Carlos <jcnvap@gmail.com>
2026-06-11 09:44:28 -0300

    fix: service id emailjs corrigido

---
+++ fix_service_id.py
@@ -0,0 +1,9 @@
+﻿# -*- coding: utf-8 -*-
+import os
+for fname in [r"index.html", r"frontend\public\landing.html"]:
+    with open(fname, "r", encoding="utf-8") as f:
+        content = f.read()
+    content = content.replace("service_htsrx6q", "service_pis825m")
+    with open(fname, "w", encoding="utf-8") as f:
+        f.write(content)
+    print(f"OK {fname} atualizado!")
