0f1ab1840839228c35694d6cad1a9b7a510a0b6f
Jose Carlos <jcnvap@gmail.com>
2026-06-11 14:47:03 -0300

    feat: botoes seletor maiores + highlight nos nomes do headline

---
+++ ver_headline.py
@@ -0,0 +1,6 @@
+﻿# -*- coding: utf-8 -*-
+path = r"index.html"
+with open(path, "r", encoding="utf-8") as f:
+    content = f.read()
+idx = content.find("hero-headline")
+print(repr(content[idx:idx+300]))
