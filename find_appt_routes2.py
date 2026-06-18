d4c7824820849c52c9a280ea7548e3eb12eb38fe
Jose Carlos <jcnvap@gmail.com>
2026-06-18 09:23:05 -0300

    chore: remove scripts temporarios

--- find_appt_routes2.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-path = r"C:\projetos\beautytech-v2\backend\src\modules\appointments\appointments.routes.ts"
-with open(path, "r", encoding="utf-8") as f:
-    lines = f.readlines()
-
-for i, line in enumerate(lines[99:140], 100):
-    print(f"{i}: {line.rstrip()}")
